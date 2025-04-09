7a4a9c7c378cb29b605429080c1a9d47d089d019
adnan <adnanadil377@gmail.com>
2025-04-09 19:01:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:gym_login/screens/OnBoardingScreen/onBoardingScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      debugShowCheckedModeBanner: false,
+      home: OnBoardingScreen(),
+    );
+  }
+}
